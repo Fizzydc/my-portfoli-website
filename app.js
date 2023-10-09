2faa9a8cdbb3fbaa50b8526a083909100c8fa817
faruq <muideenfaruq709@gmail.com>
2023-10-09 21:01:48 +0100

    new file

---
+++ app.js
@@ -0,0 +1,11 @@
+window.alert(hey whats up);
+
+alert("welcome!");
+
+var VisitorsName = prompt("what is your name");
+
+var VisitorsFavoriteColor = prompt("what is your favorite color");
+
+document.write("Hello" + VisitorsName + "Your favorite color is" + VisitorsFavoriteColor);
+
+console.log("completed")
